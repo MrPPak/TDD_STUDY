a1489b709a1a84e821554365b45aa5cfb0ab70b3
MrPPak <matht1115@naver.com>
2022-01-08 11:36:01 +0900

    feat: InputView, OutputView 완성

--- 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/view/InputView.java
+++ 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/view/InputView.java
@@ -1,4 +1,21 @@
 package study.step03_mvc.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String inputValue() {
+        return scanner.nextLine();
+    }
+
+    public String[] inputCarNames() {
+        String cars = scanner.nextLine();
+        return cars.split(",");
+    }
 }
