eca2e4419d20097f45c5222daeed0125d0c00809
MrPPak <matht1115@naver.com>
2022-01-08 12:16:34 +0900

    feat: GameController 완성

--- 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/controller/GameController.java
+++ 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/controller/GameController.java
@@ -1,6 +1,9 @@
 package study.step03_mvc.controller;
 
+import study.step03_mvc.domain.RacingGame;
+import study.step03_mvc.utils.ValidateUtils;
 import study.step03_mvc.view.InputView;
+import study.step03_mvc.view.OutputView;
 
 public class GameController {
 
@@ -10,5 +13,38 @@ public class GameController {
         this.inputView = inputView;
     }
 
+    public void play() {
+        String[] cars = inputCarNames();
+        int round = inputRacingRound();
 
+        OutputView.printRaceResultMessage();
+        RacingGame racingGame = new RacingGame(cars, round);
+        while(racingGame.isContinue()) {
+            racingGame.race();
+            OutputView.printCarStatus(racingGame.getCars());
+        }
+        OutputView.printWinner(racingGame.findWinners());
+    }
+
+    private int inputRacingRound() {
+        try {
+            OutputView.printInputNumOfRoundMessage();
+            return ValidateUtils.validateRacingRound(inputView.inputValue());
+        } catch (RuntimeException e) {
+            OutputView.printExceptionMessage(e);
+            return inputRacingRound();
+        }
+    }
+
+    private String[] inputCarNames() {
+        try {
+            OutputView.printInputCarNameMessage();
+            String[] cars = inputView.inputCarNames();
+            ValidateUtils.validateNumOfCarNames(cars);
+            return cars;
+        } catch (Exception e) {
+            OutputView.printExceptionMessage(e);
+            return inputCarNames();
+        }
+    }
 }
