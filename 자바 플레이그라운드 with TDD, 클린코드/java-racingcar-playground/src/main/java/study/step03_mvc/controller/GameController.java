f37d09e6e20586cb72c72c9f2a5887e643335e81
MrPPak <matht1115@naver.com>
2022-01-08 12:08:47 +0900

    feat: ValidateUtils 및 테스트 완료

--- 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/controller/GameController.java
+++ 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/main/java/study/step03_mvc/controller/GameController.java
@@ -1,4 +1,14 @@
 package study.step03_mvc.controller;
 
+import study.step03_mvc.view.InputView;
+
 public class GameController {
+
+    private InputView inputView;
+
+    public GameController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+
 }
--- 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/test/java/study/step03_mvc/utils/ValidateUtilsTest.java
+++ 자바 플레이그라운드 with TDD, 클린코드/java-racingcar-playground/src/test/java/study/step03_mvc/utils/ValidateUtilsTest.java
@@ -1,4 +1,39 @@
 package study.step03_mvc.utils;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.step03_mvc.exception.InvalidCarNameException;
+
 public class ValidateUtilsTest {
+
+    @Test
+    @DisplayName("자동차 이름 수 확인 검증 테스트")
+    public void numOfCarNamesTest() throws Exception {
+        // given
+        String carNames = "ppak";
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> {
+            ValidateUtils.validateNumOfCarNames(carNames.split(","));
+        }).isInstanceOf(InvalidCarNameException.class);
+    }
+
+    @Test
+    @DisplayName("시도횟수를 잘못입력할 때 검증 제대로 되는지 테스트")
+    public void validateRoundTest() throws Exception {
+        // given
+        String nonInt = "a";
+        String negativeInt = "-1";
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> {
+            ValidateUtils.validateRacingRound(nonInt);
+        }).isInstanceOf(RuntimeException.class);
+
+        Assertions.assertThatThrownBy(() -> {
+            ValidateUtils.validateRacingRound(negativeInt);
+        }).isInstanceOf(RuntimeException.class);
+
+    }
 }
